40764e23beb7e3a8e040bd5e0ec53b9e5022002b
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-11 06:00:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+end
+
+def using_pop(array)
+end
+
+def pop_with_args(array)
+end
+
+def using_shift(array)
+end
+
+def shift_with_args(array)
+end
